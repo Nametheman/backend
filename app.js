d73f86aea337d0d0556943c6b9acdad0652febcc
Emmanuel Owolabi <emmanuel@tm30.net>
2023-05-19 13:00:01 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    total: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+   
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}  `);
+});
